99c41eb5da6542eda772cf2479af22a992a221dc
Roman Kavyrshin <rom.kawirshin@yandex.ru>
2023-11-12 19:12:12 +0300

    java collection generic first attempt

--- java-collections-ru/generics/src/main/java/exercise/App.java
+++ java-collections-ru/generics/src/main/java/exercise/App.java
@@ -6,5 +6,26 @@ import java.util.ArrayList;
 import java.util.Map.Entry;
 
 // BEGIN
+class App {
+    public static List<Map<String, String>> findWhere(List<Map<String, String>> books, Map<String, String> where) {
+        var result = new ArrayList<Map<String, String>>();
+        var bookSatisfy = false;
 
+        for (Map<String, String> book : books) {
+            bookSatisfy = true;
+
+            for (Map.Entry<String, String> whereEntry : where.entrySet()) {
+                if (!book.get(whereEntry.getKey()).equals(whereEntry.getValue())) {
+                    bookSatisfy = false;
+                }
+            }
+
+            if (bookSatisfy) {
+                result.add(book);
+            }
+        }
+
+        return result;
+    }
+}
 //END
