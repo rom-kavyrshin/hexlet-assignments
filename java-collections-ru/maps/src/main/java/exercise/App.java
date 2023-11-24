d33f660f974117c7430094f3fec56829a1e4bb27
Roman Kavyrshin <rom.kawirshin@yandex.ru>
2023-11-24 21:36:24 +0300

    First commit maps assignment

---
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -0,0 +1,18 @@
+package exercise;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// BEGIN
+public class App {
+    public static Map<String, Integer> getWordCount(String sentence) {
+
+        return new HashMap<>();
+    }
+
+    public static String toString(Map<String, Integer> source) {
+
+        return "";
+    }
+}
+//END
