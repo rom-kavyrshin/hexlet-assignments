7e1ca72d580897d39b82919f2276c73e312ed111
@hexlet/cli <support@hexlet.io>
2023-11-26 17:51:23 +0300

    submit java-collections-ru/maps

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -1,18 +1,39 @@
 package exercise;
 
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 // BEGIN
 public class App {
-    public static Map<String, Integer> getWordCount(String sentence) {
 
-        return new HashMap<>();
+    public static void main(String[] args) {
+        toString(getWordCount("hi all hi some hi"));
+    }
+
+    public static Map<String, Integer> getWordCount(String sentence) {
+        return Arrays.stream(sentence.split(" "))
+                .filter(it -> !it.isEmpty())
+                .collect(Collectors.groupingBy(x -> x, Collectors.summingInt(it -> 1)));
     }
 
     public static String toString(Map<String, Integer> source) {
+        StringBuilder stringBuilder = new StringBuilder();
 
-        return "";
+        stringBuilder.append("{");
+
+        if (!source.isEmpty()) {
+            stringBuilder.append(System.lineSeparator());
+
+            for (Map.Entry<String, Integer> entry : source.entrySet()) {
+                stringBuilder.append("  ").append(entry.getKey()).append(":").append(" ").append(entry.getValue());
+                stringBuilder.append(System.lineSeparator());
+            }
+        }
+
+        stringBuilder.append("}");
+
+        return stringBuilder.toString();
     }
 }
 //END
