11ce9a3db572663def3072b0d733b651a5f77ca9
Roman Kavyrshin <rom.kawirshin@yandex.ru>
2024-06-11 16:27:38 +0300

    spring jpa-data downloaded

---
+++ java-spring-ru/jpa-data/src/main/java/exercise/model/Person.java
@@ -0,0 +1,13 @@
+package exercise.model;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
+import static jakarta.persistence.GenerationType.IDENTITY;
+import lombok.Getter;
+import lombok.Setter;
+
+// BEGIN
+
+// END
