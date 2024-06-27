3e285bea793c54dc6a50b7af0a2c88ed6233d024
Roman Kavyrshin <rom.kawirshin@yandex.ru>
2024-06-27 03:40:39 +0300

    java-spring-ru/integration-tests downloaded

---
+++ java-spring-ru/integration-tests/src/test/java/exercise/controller/TaskControllerTest.java
@@ -0,0 +1,68 @@
+package exercise.controller;
+
+import org.junit.jupiter.api.Test;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.instancio.Instancio;
+import org.instancio.Select;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+
+import java.util.HashMap;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.datafaker.Faker;
+import exercise.repository.TaskRepository;
+import exercise.model.Task;
+
+// BEGIN
+
+// END
+class ApplicationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private Faker faker;
+
+    @Autowired
+    private ObjectMapper om;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+
+    @Test
+    public void testWelcomePage() throws Exception {
+        var result = mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        var body = result.getResponse().getContentAsString();
+        assertThat(body).contains("Welcome to Spring!");
+    }
+
+    @Test
+    public void testIndex() throws Exception {
+        var result = mockMvc.perform(get("/tasks"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        var body = result.getResponse().getContentAsString();
+        assertThatJson(body).isArray();
+    }
+
+
+    // BEGIN
+    
+    // END
+}
