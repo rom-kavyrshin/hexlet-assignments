aa8d6a34cf26eaf955bd3555e0f4e7441f2ef43a
@hexlet/cli <support@hexlet.io>
2024-06-27 04:29:20 +0300

    submit java-spring-ru/integration-tests

--- java-spring-ru/integration-tests/src/test/java/exercise/controller/TaskControllerTest.java
+++ java-spring-ru/integration-tests/src/test/java/exercise/controller/TaskControllerTest.java
@@ -24,7 +24,8 @@ import exercise.repository.TaskRepository;
 import exercise.model.Task;
 
 // BEGIN
-
+@SpringBootTest
+@AutoConfigureMockMvc
 // END
 class ApplicationTest {
 
@@ -63,6 +64,92 @@ class ApplicationTest {
 
 
     // BEGIN
-    
+    @Test
+    public void testShow() throws Exception {
+        var title = faker.lorem().word();
+        var description = faker.lorem().paragraph();
+
+        var task = Instancio.of(Task.class)
+                .ignore(Select.field(Task::getId))
+                .supply(Select.field(Task::getTitle), () -> title)
+                .supply(Select.field(Task::getDescription), () -> description)
+                .create();
+
+        taskRepository.save(task);
+
+        var result = mockMvc.perform(get("/tasks/" + task.getId()))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        var body = result.getResponse().getContentAsString();
+        assertThatJson(body)
+                .isObject()
+                .containsEntry("title", title)
+                .containsEntry("description", description);
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+        var title = "Turn on lamp";
+        var description = "For better atmosphere";
+
+        var data = new HashMap<>();
+        data.put("title", title);
+        data.put("description", description);
+
+        var request = post("/tasks")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(om.writeValueAsString(data));
+
+        var result = mockMvc.perform(request)
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        var body = result.getResponse().getContentAsString();
+        var createdId = om.readValue(body, Task.class).getId();
+
+        var task = taskRepository.findById(createdId).get();
+        assertThat(task.getTitle()).isEqualTo(title);
+        assertThat(task.getDescription()).isEqualTo(description);
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        var task = Instancio.of(Task.class)
+                .ignore(Select.field(Task::getId))
+                .supply(Select.field(Task::getTitle), () -> faker.lorem().word())
+                .create();
+
+        taskRepository.save(task);
+
+        var title = "testTitle";
+
+        var data = new HashMap<>();
+        data.put("title", title);
+
+        var request = put("/tasks/" + task.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(om.writeValueAsString(data));
+
+        mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andReturn();
+
+        task = taskRepository.findById(task.getId()).get();
+        assertThat(task.getTitle()).isEqualTo(title);
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        var task = Instancio.of(Task.class)
+                .ignore(Select.field(Task::getId))
+                .create();
+
+        taskRepository.save(task);
+
+        mockMvc.perform(delete("/tasks/" + task.getId()))
+                .andExpect(status().isOk())
+                .andReturn();
+    }
     // END
 }
