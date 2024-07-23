2248c3b564763e82e709768b6b72e6a6f29ed616
Roman Kavyrshin <rom.kawirshin@yandex.ru>
2024-07-23 20:11:48 +0300

    Добавлен NotNullReferenceMapper

---
+++ java-spring-ru/crud/src/main/java/exercise/mapper/NotNullReferenceMapper.java
@@ -0,0 +1,31 @@
+package exercise.mapper;
+
+import exercise.exception.ResourceNotFoundException;
+import exercise.model.BaseEntity;
+import jakarta.persistence.EntityManager;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.TargetType;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Mapper(
+        componentModel = MappingConstants.ComponentModel.SPRING
+)
+public abstract class NotNullReferenceMapper {
+    @Autowired
+    private EntityManager entityManager;
+
+    public <T extends BaseEntity> T toEntity(Long id, @TargetType Class<T> entityClass) throws ResourceNotFoundException {
+        if (id != null) {
+            var entity = entityManager.find(entityClass, id);
+
+            if (entity == null) {
+                throw new ResourceNotFoundException(entityClass.getSimpleName() + " with id " + id + " not found");
+            }
+
+            return entity;
+        } else {
+            throw new ResourceNotFoundException(entityClass.getSimpleName() + " with id " + id + " not found");
+        }
+    }
+}
